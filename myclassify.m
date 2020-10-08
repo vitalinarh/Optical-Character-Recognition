f3a652af4d7b8316849515e6a133e07fd695bc47
Vitalina Holubenko <57838334+vitalinarh@users.noreply.github.com>
2020-10-08 12:39:43 +0100

    Update myclassify.m

--- myclassify.m
+++ myclassify.m
@@ -1,6 +1,6 @@
 function network = myclassify(data, filled)
     % Extract net
-    load('hardlim_AM_Filter.mat');
+    load('hardlim_AM_Filter_1000.mat');
     % extract result from developed network with given data
     y = net(data);
     % compute the largest elements in each column, as well as the row
