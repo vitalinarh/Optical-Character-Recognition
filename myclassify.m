5884bafe503722ec8f32d8f38967389dc25f3f79
vitalinarh <uc2017255810@student.uc.pt>
2020-10-10 19:00:49 +0100

    Updates to the interface

--- myclassify.m
+++ myclassify.m
@@ -1,8 +1,8 @@
 function network = myclassify(data, filled)
     % Extract net
-    load('hardlim_AM_Filter.mat');
+    load('hardlim_Classifier_500.mat');
     % extract result from developed network with given data
-    y = net(data);
+    y = sim(net, data);
     % compute the largest elements in each column, as well as the row
     % indices, only row indices matter
     [M, I] = max(y);
--- train.m
+++ train.m
@@ -3,6 +3,12 @@ function network = trainNetwork()
     % Choose the amount of test cases for the training dataset
     train_set = menu('Training set: ','500','1000');
     
+    % Choose the architecture for the neural network
+    architecture = menu('Architecture:', 'Filter + Classifier', 'Classifier'); 
+    
+    % Choose activation function
+    activation_function = menu('Activation Function:', 'Hardlim', 'Linear', 'Sigmoidal');
+    
     if train_set == 1
         columns = 500;
     else
@@ -30,44 +36,94 @@ function network = trainNetwork()
         target_out = horzcat(target_out, I);
     end
     
-    target_assoc_mem = repmat(Perfect, 1, columns / 10); % For associative memory 
+    % Filter + Classifier
+    if architecture == 1
+        target_assoc_mem = repmat(Perfect, 1, columns / 10); % For associative memory 
 
-    % Filter as Associative memory
-    Wp = target_assoc_mem * pinv(P);
-    P2 = Wp * P;
-    
-    net = perceptron; 
-                   
-    net = configure(net, P2, target_out);
-    
-    W = rand(10, 256);  % 256 inputs and 10 neurons
-    b = rand(10, 1);    % 1 bias value to each neuron, the value will be between 0-1
-    
-    net.IW{1, 1} = W;
-    net.b{1, 1} = b;
-    
-    % Associative Memory + Classifier
-    % hardlim activation function
-    
-    net.layers{1}.transferFcn = 'hardlim';
-    
-    net.performParam.lr = 0.01;     % learning rate| default value is 0.01
-    net.trainParam.epochs = 1000;   % The default is 1000 
-                                    % The number of epochs define the number of times that the learning algorithm will work trhough the entire training dataset. One epoch means that each sample in the training dataset has had an opportunity to update the internal model parameters
-    net.trainParam.show = 25;       % The default is 25 %show| Epochs between displays
-    net.trainParam.goal = 1e-6;     % The default is 0 %goal=objective Performance ggoal
-    net.performFcn = 'sse';         % criterion | (Sum Squared error)
-    
-    net = train(net, P2, target_out);
-    sim(net, P);
-    
-    % save network on file
-    if columns == 500
-        hardlim_AM_Filter_500 = net;
-        save hardlim_AM_Filter_500;
-    else
-        hardlim_AM_Filter_1000 = net;
-        save hardlim_AM_Filter_1000;
+        % Filter as Associative memory
+        Wp = target_assoc_mem * pinv(P);
+        P2 = Wp * P;
+
+        net = perceptron; 
+
+        net = configure(net, P2, target_out);
+
+        W = rand(10, 256);  % 256 inputs and 10 neurons
+        b = rand(10, 1);    % 1 bias value to each neuron, the value will be between 0-1
+
+        net.IW{1, 1} = W;
+        net.b{1, 1} = b;
+        
+        % Associative Memory + Classifier
+        % hardlim activation function
+        if activation_function == 1
+
+            net.layers{1}.transferFcn = 'hardlim';
+
+            net.performParam.lr = 0.01;     % learning rate| default value is 0.01
+            net.trainParam.epochs = 50;     % The default is 1000 
+                                            % The number of epochs define the number of times that the learning algorithm will work trhough the entire training dataset. One epoch means that each sample in the training dataset has had an opportunity to update the internal model parameters
+            net.trainParam.show = 25;       % The default is 25 %show| Epochs between displays
+            net.trainParam.goal = 1e-6;     % The default is 0 %goal=objective Performance goal
+            net.performFcn = 'sse';         % criterion | (Sum Squared error)
+
+            net = train(net, P2, target_out); 
+
+            % Pt = load('P1.mat');
+            % a = sim(net, Pt.P);
+
+            % save network on file
+            if columns == 500
+                hardlim_AM_Filter_500 = net;
+                save hardlim_AM_Filter_500;
+            else
+                hardlim_AM_Filter_1000 = net;
+                save hardlim_AM_Filter_1000;
+            end
+        % linear activation function
+        elseif activation_function == 2
+        % sigmoidal activation function
+        else
+        end
+    % Only Classifier
+    else 
+        net = perceptron; 
+        net = configure(net, P, target_out);
+
+        W = rand(10, 256);  % 256 inputs and 10 neurons
+        b = rand(10, 1);    % 1 bias value to each neuron, the value will be between 0-1
+
+        net.IW{1, 1} = W;
+        net.b{1, 1} = b;
+        
+        % hardlim activation function
+        if activation_function == 1
+            net.layers{1}.transferFcn = 'hardlim';
+
+            net.performParam.lr = 0.01;   %learning rate| default value is 0.01
+            net.trainParam.epochs = 100;  %The default is 1000 %The number of epochs define the number of times that the learning algorithm will work trhough the entire training dataset. One epoch means that each sample in the training dataset has had an opportunity to update the internal model parameters
+            net.trainParam.show = 35;     %The default is 25 %show| Epochs between displays
+            net.trainParam.goal = 1e-6;   %The default is 0 goal=objective Performance goal
+            net.performFcn = 'sse';       %criterion | (Sum Squared error)
+
+            net = train(net, P, target_out);
+            
+            % Pt = load('P1.mat');
+            % a = sim(net, Pt.P);
+
+            % save network on file
+            if columns == 500
+                hardlim_Classifier_500 = net;
+                save hardlim_Classifier_500;
+            else
+                hardlim_Classifier_1000 = net;
+                save hardlim_Classifier_1000;
+            end
+        % linear activation function
+        elseif activation_function == 2
+        % sigmoidal activation function
+        else
+        end
     end
     
     return
